3ad0e51030ecc62f8e0b9b3dab1c9c7c221dbf62
Andrei Khrapavitski <andrei.khrapavitski@gmail.com>
2020-09-27 19:05:15 +0300

    fix all_tests_seconds_taken

--- run.py
+++ run.py
@@ -236,7 +236,8 @@ for desired_cap in desired_cap_list:
             print ("Time taken: " + str(this_test_seconds_taken) + " seconds")
 
     # Output the amount of time it took all tests to run on the current platform
-    print(f"--------------------------------------------------\n all_tests_seconds_taken = ({time.time()} - all_tests_start_time)")
+    print(f"--------------------------------------------------\n") 
+    all_tests_seconds_taken = (time.time() - all_tests_start_time)
     if(all_tests_seconds_taken > 60):
         print ("Time taken for all tests: " + str(all_tests_seconds_taken / 60) + " minutes")
     else:
